e3f1b83cbfefaedd50135bac9d55b58187d65618
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-04-04 16:54:18 -0500

    added the App.js logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Grid } from "@mui/material";
+import TimeZoneCard from "./components/TimeZoneCard";
+
+const timezones = [
+  { timezone: 'America/New_York', timezoneName: 'Eastern Time Zone (EST)' },
+  { timezone: 'America/Chicago', timezoneName: 'Central Time Zone (CST)' },
+  { timezone: 'America/Denver', timezoneName: 'Mountain Time Zone (MST)' },
+  { timezone: 'America/Los_Angeles', timezoneName: 'Pacific Time Zone (PST)' },
+  { timezone: 'America/Anchorage', timezoneName: 'Alaska Time Zone (AKST)' },
+  { timezone: 'Pacific/Honolulu', timezoneName: 'Hawaii-Aleutian Time Zone (HST)' },
+  { timezone: 'Pacific/Midway', timezoneName: 'Samoa Time Zone (SST)' },
+  { timezone: 'Philipines', timezoneName: 'Philippine Time Zone (PHT)' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        {timezones.map((tz, index) => (
+          <Grid>
+            <TimeZoneCard timezone={tz.timezone} timezoneName={tz.timezoneName} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
